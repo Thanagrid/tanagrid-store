24a73e8ee2572ad02d354182f03fcd792c6cb8ef
Thanagrid Srikaew <vrzo20_@hotmail.co.th>
2025-08-02 14:37:15 +0700

    day23.2

--- features/products/component/product-form.tsx
+++ features/products/component/product-form.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import {
   Card,
   CardContent,
@@ -21,12 +23,39 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { CategoryType } from '@/types/categories'
+import { useForm } from '@/hook/use-form'
+import { productAction } from '../action/product'
+import ErrorMessage from '@/components/shared/errorMessage'
 
 interface ProductFormProps {
    categories: CategoryType[]
 }
 
 const ProductForm = ({categories}: ProductFormProps) => {
+
+   const [basePrice, setBasePrice] = useState("")
+   const [salePrice, setSalePrice] = useState("")
+
+   const {errors, formAction, isPending, clearErrors} = useForm(productAction, "/admin/products")
+
+   const calulateDiscount = () => {
+      const basePriceNum = parseFloat(basePrice) || 0;
+      const salePriceNum = parseFloat(salePrice) || 0;
+
+      if(basePriceNum === 0 || salePriceNum === 0){
+         return '0%'
+      }
+
+      if(basePriceNum <= salePriceNum){
+         return '0%'
+      }
+
+      const discount = ((basePriceNum - salePriceNum) / basePriceNum ) * 100
+
+
+      return `${discount.toFixed(2)}%`
+   }
+
    return (
       <Card className='max-w-4xl mx-auto'>
          <CardHeader>
@@ -37,7 +66,7 @@ const ProductForm = ({categories}: ProductFormProps) => {
                Enter the details of your new product
             </CardDescription>
          </CardHeader>
-         <Form action=''>
+         <Form action={formAction} onChange={clearErrors}>
             <CardContent className='flex flex-col gap-4'>
                {/* Basic Information */}
                <div className='flex flex-col gap-4'>
@@ -45,6 +74,7 @@ const ProductForm = ({categories}: ProductFormProps) => {
                   {/* Title */}
                   <div className='flex flex-col gap-2'>
                      <InputForm label='Product Title' id='title' placeholder='Enter product title' required></InputForm>
+                     {errors.title && <ErrorMessage errors={errors.title[0]} />}
                   </div>
                   {/* Descript */}
                   <div className='flex flex-col gap-2'>
@@ -52,6 +82,7 @@ const ProductForm = ({categories}: ProductFormProps) => {
                         Description <span className='text-red-500'>*</span>
                      </Label>
                      <Textarea name="description" id="Description" placeholder='enter product description' className='min-h-20 w-full'></Textarea>
+                     {errors.description && <ErrorMessage errors={errors.description[0]} />}
                   </div>
                   {/* category */}
                   <div className='flex flex-col gap-2'>
@@ -68,6 +99,7 @@ const ProductForm = ({categories}: ProductFormProps) => {
                            ))}
                         </SelectContent>
                      </Select>
+                     {errors.categoryId && <ErrorMessage errors={errors.categoryId[0]} />}
                   </div>
                </div>
 
@@ -75,16 +107,38 @@ const ProductForm = ({categories}: ProductFormProps) => {
 
                <div className='flex flex-col gap-4'>
                   <h3 className='font-medium'>Pricing Information</h3>
-                  <div>Input</div>
+                  <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+                     <div className='flex flex-col gap-2'>
+                        <InputForm label='Cost Price' id='cost' type="number" min="0" step="any" placeholder='0.00'></InputForm>
+                     </div>
+                     {/* Base Price */}
+                     <div className='flex flex-col gap-2'>
+                        <InputForm label='Base Price' id='base-price' type="number" min="0" step="any" placeholder='0.00' required value={basePrice} onChange={(event) => setBasePrice(event.target.value)}></InputForm>
+                            {errors.basePrice && (
+                              <ErrorMessage errors={errors.basePrice[0]} />
+                           )}
+                     </div>
+                     <div className='flex flex-col gap-2'>
+                        <InputForm label='Sale Price' id='price' type="number" min="0" step="any" placeholder='0.00' required value={salePrice} onChange={(event) => setSalePrice(event.target.value)}></InputForm>
+                        {errors.price && <ErrorMessage errors={errors.price[0]} />}
+                     </div>
+                     <div className='flex flex-col gap-2'>
+                        <Label>Discount</Label>
+                        <div className='h-9 px-3 rounded-md border border-input bg-gray-50 flex items-center'>{calulateDiscount()}</div>
+                     </div>
+                  </div>
                </div>
 
                <div className='flex flex-col gap-4'>
                   <h3 className='font-medium'>Stock Information</h3>
-                  <div>Input</div>
+                  <div>
+                     <InputForm label='Stock Quantity' id='stock' type='number' min='0' step="1" placeholder='0' required></InputForm>
+                  </div>
+                  {errors.stock && <ErrorMessage errors={errors.stock[0]} />}
                </div>
 
                <CardFooter>
-                  <SubmitBtn name='save porduct' icon={Save} className='w-full'></SubmitBtn>
+                  <SubmitBtn name='save porduct' icon={Save} className='w-full' pending={isPending}></SubmitBtn>
                </CardFooter>
             </CardContent>
          </Form>
--- features/products/db/products.ts
+++ features/products/db/products.ts
@@ -1,6 +1,27 @@
 import { prisma } from "@/lib/db";
+import { unstable_cacheLife, unstable_cacheTag } from "next/cache";
+import { getProductGlobalTag, revalidateProductCache } from "./cache";
+import { productSchema } from "../schemas/products";
+import { authCheck } from "@/features/auths/db/auth";
+import { canCreateProduct } from "../permission/product";
+import { redirect } from "next/navigation";
+
+interface CreateProductInput {
+  title: string;
+  description: string;
+  cost?: number;
+  basePrice: number;
+  price: number;
+  stock: number;
+  categoryId: string;
+}
 
 export const getProduct = async () => {
+   'use cache'
+
+   unstable_cacheLife("hours")
+   unstable_cacheTag(getProductGlobalTag())
+
    try {
       const products = await prisma.product.findMany({
          include: {
@@ -18,7 +39,7 @@ export const getProduct = async () => {
          return {
                ...product,
                lowStock: 5,
-               sku: product.id.substring(0,8)
+               sku: product.id.substring(0,8).toUpperCase(),
             }
       })
 
@@ -27,5 +48,57 @@ export const getProduct = async () => {
    }catch(err){
       console.log(err);
       return [];
+   }
+}
+
+export const createProduct = async (input: CreateProductInput) => {
+
+   const user = await authCheck();
+   if(!user || !canCreateProduct){
+      redirect('/')
+   }
+
+   try {
+      const { success, data, error } = productSchema.safeParse(input);
+   
+      if(!success){
+         return {
+            message: 'Please enter valid product information',
+            error: error.flatten().fieldErrors
+         }
+      }
+
+      const category = await prisma.category.findUnique({
+         where: {
+            id: data.categoryId,
+            status: 'Active'
+         }
+      })
+
+      if(!category) {
+         return {
+            message: 'selected category not found or inactive'
+         }
+      }
+
+      const newProduct = await prisma.product.create({
+         data: {
+            title: data.title,
+            description: data.description,
+            cost: data.cost,
+            basePrice: data.basePrice,
+            price: data.price,
+            stock: data.stock,
+            categoryId: data.categoryId
+         }
+      })
+
+      revalidateProductCache(newProduct.id);
+   
+   }catch(err){
+      console.log(err);
+      return {
+         message: "Somthing went wrong."
+      }
    }
 }
